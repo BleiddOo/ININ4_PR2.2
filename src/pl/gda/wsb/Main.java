febdb10768e7fa2f849c83415d96f0b388b39c75
Grzegorz Filipiak <g_filipiak@o2.pl>
2021-02-28 13:28:40 +0100

    nowe obiekty

--- src/pl/gda/wsb/Main.java
+++ src/pl/gda/wsb/Main.java
@@ -7,11 +7,17 @@ public class Main {
     public static void main(String[] args) {
 	    System.out.println("Hello world");
 	    System.out.println("Hello world2");
-        Animal zwierze = new Animal("Pies","Ssak");
+        Animal zwierze1 = new Animal("Pies","Ssak");
+        Animal zwierze2 = new Animal("Kot","Ssak");
         Human czlowiek1 = new Human("Adam",22,"niebieski");
+        Human czlowiek2 = new Human("Karol",30,"zielony");
         Phone telefon1 = new Phone("iPhone", "czerwony", 5.5);
+        Phone telefon2 = new Phone("Samsung", "szary", 5.7);
         System.out.println("Imie: " + czlowiek1.imie);
         System.out.println("Model telefonu: " + telefon1.model);
-        System.out.println("Rodzaj zwierzecia: " + zwierze.nazwaZwierzecia);
+        System.out.println("Rodzaj zwierzecia: " + zwierze1.nazwaZwierzecia);
+        System.out.println("Imie: " + czlowiek2.imie);
+        System.out.println("Model telefonu: " + telefon2.model);
+        System.out.println("Rodzaj zwierzecia: " + zwierze2.nazwaZwierzecia);
     }
 }
