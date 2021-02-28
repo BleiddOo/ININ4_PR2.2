b4437e031785e857d9be41f84b9d1bb1e3157470
Grzegorz Filipiak <g_filipiak@o2.pl>
2021-02-28 13:50:07 +0100

    nowe pole "waga"

--- src/pl/gda/wsb/Animal.java
+++ src/pl/gda/wsb/Animal.java
@@ -3,9 +3,11 @@ package pl.gda.wsb;
 public class Animal {
     String nazwaZwierzecia;
     String gatunek;
+    int waga;
 
-    public Animal(String nazwaZwierzecia, String gatunek) {
+    public Animal(String nazwaZwierzecia, String gatunek,int waga) {
         this.nazwaZwierzecia = nazwaZwierzecia;
         this.gatunek = gatunek;
+        this.waga = waga;
     }
 }
