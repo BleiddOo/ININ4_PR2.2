d01f4058f4297d77e607778ed51b057952faec6c
Grzegorz Filipiak <g_filipiak@o2.pl>
2021-02-28 14:30:49 +0100

    dodanie StringBuilder'a i listy

--- src/pl/gda/wsb/Main.java
+++ src/pl/gda/wsb/Main.java
@@ -1,6 +1,7 @@
 package pl.gda.wsb;
 
 import java.sql.SQLOutput;
+import java.util.ArrayList;
 
 public class Main {
 
@@ -20,14 +21,28 @@ public class Main {
         System.out.println("Model telefonu: " + telefon2.producent);
         System.out.println("Rodzaj zwierzecia: " + zwierze2.nazwaZwierzecia);
 
-        if(zwierze1.waga > zwierze2.waga){
-            System.out.println("Ciezszy jest: " + zwierze1.nazwaZwierzecia + ". Jego waga to: "+zwierze1.waga+" kg.");
+        if(zwierze1.waga > zwierze2.waga) {
+            //System.out.println("Ciezszy jest: " + zwierze1.nazwaZwierzecia + ". Jego waga to: "+zwierze1.waga+" kg.");
+            StringBuilder wagaCase1 = new StringBuilder("Ciezszy jest: ").append(zwierze1.nazwaZwierzecia).append(". Jego waga to: ").append(zwierze1.waga).append(" kg.");
+            System.out.println(wagaCase1);
         }
         else if(zwierze1.waga < zwierze2.waga){
-            System.out.println("Ciezszy jest: " + zwierze2.nazwaZwierzecia + ". Jego waga to: "+zwierze2.waga+" kg.");
+            //System.out.println("Ciezszy jest: " + zwierze2.nazwaZwierzecia + ". Jego waga to: "+zwierze2.waga+" kg.");
+            StringBuilder wagaCase2 = new StringBuilder("Ciezszy jest: ").append(zwierze2.nazwaZwierzecia).append(". Jego waga to: ").append(zwierze2.waga).append(" kg.");
+            System.out.println(wagaCase2);
         }
         else{
-            System.out.println("Zwierzeta waza tyle samo: " + zwierze1.waga + "kg");
+            //System.out.println("Zwierzeta waza tyle samo: " + zwierze1.waga + "kg");
+            StringBuilder wagaCase3 = new StringBuilder("Zwierzeta waza tyle samo: ").append(zwierze1.waga).append("kg");
+            System.out.println(wagaCase3);
+        }
+
+        ArrayList<Human> humanslist = new ArrayList<>();
+        humanslist.add(czlowiek1);
+        humanslist.add(czlowiek2);
+
+        for(Human human : humanslist){
+            System.out.println(human.imie);
         }
     }
 }
